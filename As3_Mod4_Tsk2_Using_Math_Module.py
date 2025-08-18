13cd375ad448121f09cb305c18537c14c5653fb3
Ravie S. Tiwari <ravistiwarix@gmail.com>
2025-08-18 09:14:48 +0530

    Add files via upload

---
+++ As3_Mod4_Tsk2_Using_Math_Module.py
@@ -0,0 +1,11 @@
+# This program takes a number input from the user
+# Using the math module calculates: Square root, Natural log, Sin of the input number
+# Displays the calculated results.
+
+import math
+
+num=int(input("Enter number: "))
+
+print("Square root: ", math.sqrt(num))
+print("Logarithm: ", math.log(num))
+print("Sine: ", math.sin(num))
